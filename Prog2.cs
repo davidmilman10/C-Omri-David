7097ee882ad7bece37c05cdcc5943c8a365ca23c
davidmilman10 <68965363+davidmilman10@users.noreply.github.com>
2023-03-22 09:03:21 +0200

    Add files via upload

---
+++ Prog2.cs
@@ -0,0 +1,46 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace targil1
+    {
+        internal class Prog2
+        {
+            static void Main(string[] args)
+            {
+                int[] arr = new int[9];
+                int[] arr2 = new int[9];
+                int i = 0, j = 0;
+                Console.WriteLine("Please Enter Numbers for the array between 0-100: ");
+                for (i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = int.Parse(Console.ReadLine());
+                    while (arr[i] < 0 || arr[i] > 100)
+                    {
+                        Console.Write("Number Not in Range please re enter: ");
+                        arr[i] = int.Parse(Console.ReadLine());
+                    }
+                }
+                for (i = 0; i < arr.Length; i++)
+                {
+                    if (Array.IndexOf(arr, arr[i]) == Array.LastIndexOf(arr, arr[i]))
+                    {
+                        arr2[j] = arr[i]; j++;
+                    }
+                }
+                Console.WriteLine("Array2 elements are: ");
+                for ( i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine(+arr2[i] + " ");
+
+                }
+        
+         
+
+            }
+
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace targil1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string employ="Omri";
+//            int dollar=0;
+//            double shekel, commision,price,payment;
+//            Console.WriteLine("Welcome to " + employ + "'s Store");
+//            string customer;
+//            Console.WriteLine("Please enter your name: ");
+//            customer = Console.ReadLine();
+//            Console.WriteLine("Please enter the Amount in US$ you want: ");
+//            dollar = int.Parse(Console.ReadLine());
+//            shekel = 3.42 * dollar;
+//            commision = 0.015 * shekel;
+//            price = shekel + commision;
+//            payment = commision / 3;
+//            Console.WriteLine( "    #The Amount of Dollars you get is: " + dollar + "\n    #The Price in Shekels is: " + shekel + "\n    #The price of commission is: " +commision+ "\n    #The fee in one payment is: " +payment+
+//              "\n    #The fee in three payment is " + (payment * 3) + 
+//                "\n    #The total price is: " + price);
+
+
+//        }
+//    }
+//}
